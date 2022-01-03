278877f31b7124f1f585db85044e23c05de508df
Henry-Cho <hyungseok5926@gmail.com>
2022-01-03 11:44:22 -0700

    Initial check-in of module MyFirstASPNET

---
+++ MyFirstASPNET/wwwroot/js/fanMail.js
@@ -0,0 +1,6 @@
+﻿$("#btn").click(() => {
+    let from = $("#from").val();
+    let sub = $("#sub").val();
+    let txt = $("#txt").val();
+    alert("From: " + from + "," + "\n" + "Subject: " + sub + "\n" + "Message: " + txt);
+})
