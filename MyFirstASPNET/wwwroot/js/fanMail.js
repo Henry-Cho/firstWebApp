f5aaebce3018a88852604bdd2aec2465b5d18054
Henry-Cho <hyungseok5926@gmail.com>
2022-01-04 08:11:52 -0700

    * index.html: * favi.webp: * style.css: * enjoyingFamTime.jpeg:
    * fanMail.js: Changes for now.

--- MyFirstASPNET/wwwroot/js/fanMail.js
+++ MyFirstASPNET/wwwroot/js/fanMail.js
@@ -1,6 +1,51 @@
 ﻿$("#btn").click(() => {
-    let from = $("#from").val();
-    let sub = $("#sub").val();
-    let txt = $("#txt").val();
-    alert("From: " + from + "," + "\n" + "Subject: " + sub + "\n" + "Message: " + txt);
+    let hw = $("#hw").val() * 0.55;
+    let gp = $("#gp").val() * 0.05;
+    let quiz = $("#quiz").val() * 0.1;
+    let exam = $("#exam").val() * 0.2;
+    let intex = $("#intex").val() * 0.1;
+
+    let total = hw + gp + quiz + exam + intex;
+
+    let letterGrade = "";
+
+    if (total >= 94) {
+        letterGrade = "A";
+    }
+    else if (total >= 90) {
+        letterGrade = "A-";
+    }
+    else if (total >= 87) {
+        letterGrade = "B+";
+    }
+    else if (total >= 84) {
+        letterGrade = "B";
+    }
+    else if (total >= 80) {
+        letterGrade = "B-";
+    }
+    else if (total >= 77) {
+        letterGrade = "C+";
+    }
+    else if (total >= 74) {
+        letterGrade = "C";
+    }
+    else if (total >= 70) {
+        letterGrade = "C-";
+    }
+    else if (total >= 67) {
+        letterGrade = "D+";
+    }
+    else if (total >= 64) {
+        letterGrade = "D";
+    }
+    else if (total >= 60) {
+        letterGrade = "D-";
+    }
+    else {
+        letterGrade = "E";
+    }
+
+    $("#tp").html(total + "%");
+    $("#lg").html(letterGrade);
 })
